03e0eec7120a96750d27093d22fd35c0571e38a7
AdMis01 <72755308+AdMis01@users.noreply.github.com>
2024-08-04 16:43:32 +0200

    test
    test

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # JS
+
+Tutaj wrzucamy stare projekty i jakieś inne duperele jak ktoś chce sobie zobaczyć czy zapożyczuć
---
+++ tenery_operator/index.js
@@ -0,0 +1,14 @@
+let wiek = 16;
+let komunikat = "";
+
+if(wiek > 12){
+    komunikat = "do gimbazy";
+}else{
+    komunikat = "do podstawówki";
+}
+
+console.log(komunikat);
+
+let komunikatWarunku = wiek > 12 ? "do gimbazy" : "do podstawówki";
+
+console.log(komunikatWarunku);
