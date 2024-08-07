967bb5cab28a561643ae863603b6e7fcce7a7ffb
AdMis01 <72755308+AdMis01@users.noreply.github.com>
2024-08-07 17:18:13 +0200

    Szablon
    Dodanie szablony do strony

---
+++ responsive_stron_szablon/script.js
@@ -0,0 +1,16 @@
+const toggle = document.getElementById("toggle");
+const navbar = document.getElementById("navbar");
+const header = document.getElementById("header");
+
+document.onclick = function(e){
+    if(e.target.id!=='header' && e.target.id!=='toggle' && e.target!=='navbar')
+    {
+        toggle.classList.remove("active");
+        navbar.classList.remove("active");
+    }
+}
+
+toggle.onclick=function(){
+    toggle.classList.toggle("active");
+    navbar.classList.toggle("active");
+}
