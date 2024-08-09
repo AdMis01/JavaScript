3bc0ff53911e918eea55fc9726dfcd361d9d7d10
AdMis01 <72755308+AdMis01@users.noreply.github.com>
2024-08-09 12:28:51 +0200

    dodanie kolejnego
    dodanie poprawek

--- tablice/index.js
+++ tablice/index.js
@@ -29,6 +29,8 @@ for(let tab of tablica){
 //console.log(tablica[3]);
 //console.log(tablica[4]);
 
+//spread rozszerza tablice / tworzy wieksza tablicy z wielu elementow
+
 let numery = [1,2,3,4,5];
 let maks = Math.max(...numery);
 let mini = Math.min(...numery);
@@ -40,3 +42,17 @@ console.log(kazdaLitera);
 let polaczona = [...numery,...tablica,"200"];
 
 console.log(polaczona);
+
+//rest parameter laczy elementy w jedna tablice
+
+function wyswietalnie(...elementy){
+    console.log(elementy);
+}
+
+const food1 = "jeden";
+const food2 = "dwa";
+const food3 = "trzy";
+const food4 = "cztery";
+const food5 = "piec";
+
+wyswietalnie(food1,food2,food3,food4,food5);
