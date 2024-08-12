9ab9806fa30df654c5616ab61967dcf6c283b3e6
AdMis01 <72755308+AdMis01@users.noreply.github.com>
2024-08-12 15:58:31 +0200

    dodawanie
    dodanie nowego bloku

--- 12_dodatkowe_rzeczy_esp6/index.js
+++ 12_dodatkowe_rzeczy_esp6/index.js
@@ -36,4 +36,105 @@ function wyswietl(elementy){
     console.log(elementy);
 }
 
+//.map() - zwraca nowa tablice po przejsciu funkcji
 
+const numerki = [1,2,3,4,5];
+
+const podwojone = numerki.map(podwoj);
+
+const rozmiary = numerki.map(function(e){
+    return e + e;
+})
+
+console.log(podwojone);
+console.log(rozmiary);
+
+function podwoj(elem){
+    return Math.pow(elem,2);
+}
+
+//.filter() - zwraca przefiltrowana tablice i zwraca konkrete elementy ktore chcemy
+
+let numer = [1,2,3,4,5];
+
+let evenNumbers = numer.filter(isEven);
+
+let miniLiczba4 = numer.filter(function(el){
+    return el >= 4;
+});
+
+console.log(evenNumbers);  
+console.log(miniLiczba4);  
+
+function isEven(element){
+    return element % 2 === 0;
+}
+
+//.reduce() - zwraca wartosci z tablicy do jedenj wartosci 
+
+const ceny = [15,5,30,55,60];
+
+const wszystko = ceny.reduce(suma);
+const maksimum = ceny.reduce(max);
+
+console.log(wszystko.toFixed(2)+" $");
+console.log(maksimum);
+
+function suma(kontener,element){
+    return kontener + element;
+}
+
+function max(kontener,element){
+    return Math.max(kontener,element); 
+}
+
+//function declaration
+
+function deklarowana(){
+    console.log("komunikat1");
+}
+
+deklarowana();
+
+//function expressions
+let komunikat = function(){
+    console.log("komunikat2");
+}
+
+komunikat();
+
+setTimeout(deklarowana,3000);
+
+setTimeout(function(){
+    console.log("przekazywanie calej funkcji jako ");
+},3000);
+
+const imiona = ["adam","ewa"];
+
+const imi = imiona.map(function(element){
+    return element.toUpperCase();
+});
+
+console.log(imi);
+
+//arrow functions - funkcja strzalkowa dobra do stosowania do funkcji co wykona sie tylko raz
+
+const hello = () => {
+    console.log("hello");
+}
+const witam = (imie) => {
+    console.log(`siema ${imie}`);
+}
+
+hello();
+hello("Adam");
+
+setTimeout(() => {
+    console.log("arrow function");
+},3000);
+
+const squ = numerki.filter((element) => {
+    return element % 2 === 0;//jak jest tylko jedna linia to nie potrzeba dawac return
+});
+
+console.log(squ);
