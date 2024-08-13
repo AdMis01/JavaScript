fa4dcebfe25dc3484714d5954dd2c106c70883ad
AdMis01 <72755308+AdMis01@users.noreply.github.com>
2024-08-13 21:16:03 +0200

    dodawanie
    zawansowane tablice obiekty itp

--- 14_zawansowane_tablice/script.js
+++ 14_zawansowane_tablice/script.js
@@ -189,3 +189,23 @@ const people = [
 people.sort((a,b) => { return a.age.toLocaleString(b.age)});//sortuje po imieniu po stringu 
 
 console.log(people);
+
+const cards = ['A',2,3,4,5,6,7,8,9,10,'J','Q','K'];
+
+//cards.sort(() => {return Math.random() - 0.5}); // sortuje to randomowo
+
+//console.log(cards);
+
+//Fisher-Yates algorytm 
+shuffle(cards);
+
+console.log(cards);
+
+function shuffle(array){
+    for(let i = array.length - 1; i > 0; i--){
+        const random = Math.floor(Math.random() * (i + 1));
+
+        [array[i], array[random]] = [array[random], array[i]]; 
+    }
+}
+
