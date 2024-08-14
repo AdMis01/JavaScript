aa38df32abb100e2681dd14056bef585cd919ea9
AdMis01 <72755308+AdMis01@users.noreply.github.com>
2024-08-14 14:54:44 +0200

    stoper
    dodanie stoppera

---
+++ 16_stopwatch/script.js
@@ -0,0 +1,50 @@
+const display = document.getElementById("display");
+
+let timer = null;
+let startTime=0;
+let elapsedTime = 0;
+let isRunning = false;
+
+function start(){
+    if(!isRunning){
+        startTime = Date.now() - elapsedTime;
+        timer = setInterval(update,10);
+        isRunning = true;
+    }
+
+
+    //console.log(startTime);
+}
+
+function stop(){
+    if(isRunning){
+        clearInterval(timer);
+        //elapsedTime = Date.now() - startTime;
+        isRunning = false;
+    }
+}
+
+function reset(){
+    clearInterval(timer);
+    startTime = 0;
+    elapsedTime = 0;
+    isRunning = false;
+    display.textContent = "00:00:00:00";
+}
+
+function update(){
+    const curentTime = Date.now();
+    elapsedTime = curentTime - startTime;
+
+    let hours =Math.floor(elapsedTime / (1000 * 60 * 60));
+    let minutes = Math.floor(elapsedTime / (1000 * 60) % 60);
+    let seconds = Math.floor(elapsedTime / 1000 % 60);
+    let miliseconds = Math.floor(elapsedTime % 1000 / 10);
+
+    hours = String(hours).padStart(2,0);
+    minutes = String(minutes).padStart(2,0);
+    seconds = String(seconds).padStart(2,0);
+    miliseconds = String(miliseconds).padStart(2,0);
+
+    display.textContent = `${hours}:${minutes}:${seconds}:${miliseconds}`;
+}
